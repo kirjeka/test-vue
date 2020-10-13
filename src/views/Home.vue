8fa51c714e0e4391812dab7134d835fadc150663
Evgeniy Kiryanow <zhenia.galanix@gmail.com>
2020-10-13 16:26:46 +0300

    popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="home">
     <h1 style="height: 850px">HomePage</h1>
-    
-
+    <v-btn @click="showPopup" elevation="2" raised rounded style="color: aqua"
+      >Add Card</v-btn
+    >
+    <div class="test1" v-if="!show"></div>
+    <div class="test2" v-else>
+      <v-card
+        style="width: 250px;height: 250px;position: fixed;top: 50%;right: 50%"
+      >
+        Test-card
+        <v-btn @click="closePopup">Close</v-btn>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,19 @@
 // @ is an alias to /src
 
 export default {
-  name: "Home"
+  name: "Home",
+  data: function() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    showPopup() {
+      this.show = true;
+    },
+    closePopup(){
+      this.show = false;
+    }
+  }
 };
 </script>
